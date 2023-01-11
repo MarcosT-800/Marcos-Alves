088f8d7a2b01fe1501e94c697a553615ad3f077a
Marcos Alves <marcosalvesjr007@gmail.com>
2023-01-11 20:15:25 +0000

    1-criação do modelo de pasta do blog

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Head from 'next/head'
+
+import RocketseatLogo from '../assets/rocketseat.svg'
+
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <RocketseatLogo />
+      <h1>Blog</h1>
+      <p>Post novos todo sabádo</p>
+
+    </Container>
+  )
+}
+
+export default Home
